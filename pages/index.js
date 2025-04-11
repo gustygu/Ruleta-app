c8d3c5dbebc03738eeb16e63c6c7b95b0bdbbf2d
gustygu <gussan241@gmail.com>
2025-04-11 19:31:41 -0300

    Update index.js
    Modifico nombre

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import RuletaSimulador from '../components/RuletaSimulador';
 
 export default function Home() {
   return (
-    <main style={{ padding: '1rem' }}>
+    <main>
       <RuletaSimulador />
     </main>
   );
